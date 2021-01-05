a8114d290be24a604a52df7015a11d7c46cb34e4
Sebastian_Lares <sebastian.laresh@gmail.com>
2021-01-05 01:49:02 -0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.css";
 
 function App() {
-  let html = true;
-  let nivel = "avanzado";
-  nivel === "avanzado" ? (html = true) : (html = false);
-  return <div className="App">{console.log(html)}</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
